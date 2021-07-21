8c584ecbfedc9d498edd3253bd3a7112ce1cf512
= <=>
2021-07-21 10:33:44 +0100

    Chapter 32 completed

---
+++ 32_Advanced/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Advanced.Models;
+using Advanced.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Advanced.Controllers
+{
+    public class HomeController : Controller
+    {
+        private DataContext context;
+
+        public HomeController(DataContext dbContext)
+        {
+            context = dbContext;
+        }
+
+        public IActionResult Index([FromQuery] string selectedCity)
+        {
+            PeopleListViewModel vm = new PeopleListViewModel {
+                People = context.People
+                .Include(p => p.Department)
+                .Include(p => p.Location
+                
+                ),
+                Cities = context.Locations.Select(l => l.City).Distinct(),
+                SelectedCity = selectedCity
+
+            };
+            return View(vm);
+        }
+    }
+
+}
